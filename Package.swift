313211cf1f5a155f4e181b4fb48d6f6e81e6f283
Frizlab <fload@me.com>
2024-02-11 02:05:18 +0100

    Move the sources one level up

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
 	targets: [
 		.target(name: "APIConnectionProtocols", dependencies: [
 			.product(name: "TaskQueue", package: "swift-task-queue")
-		])
+		], path: "Sources")
 	]
 )
